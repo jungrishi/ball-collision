35ae9bb0f5d39e89982eb5ff9d55afd292624cab
jungrishi <ranabhat.85@gmail.com>
2019-06-24 09:57:29 +0545

    not yet collision

--- js/check.js
+++ js/check.js
@@ -1,15 +1,17 @@
 const WRAPPERWIDTH = 700;
 const WRAPPERHEIGHT = 500;
-const BOXHEIGHT = 10;
-const BOXWIDTH = 10;
-const TOTALBOX = 5;
+const BOXHEIGHT = 35;
+const BOXWIDTH = 35;
+const TOTALBOX = 10;
 const GRAVITY = 9.81;
+const DIAGONAL =Math.sqrt(Math.pow(BOXWIDTH,2)+Math.pow(BOXHEIGHT,2));
 
 var wrapper = document.getElementById('app-wrapper');
 wrapper.style.width = WRAPPERWIDTH + 'px';
 wrapper.style.height = WRAPPERHEIGHT + 'px';
 wrapper.style.position = 'relative';
 wrapper.style.border = 'solid 1px black'; 
+
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
@@ -29,16 +31,17 @@ function Box(x, y, parentEle,xVel, yVel){
     this.element.setAttribute('class', 'box');
     this.element.style.width = BOXWIDTH + 'px';
     this.element.style.height = BOXHEIGHT + 'px';
-    this.element.style.backgroundColor = 'red';
+    this.element.style.backgroundColor = 'black';
     this.element.style.position = 'absolute';
-    this.element.style.color = this.color;
     console.log(this.element);
     parentEle && parentEle.appendChild(this.element);
   }
 
   this.draw = function() {
+    console.log('in draw')
     this.element.style.top = this.y + 'px';
     this.element.style.left = this.x + 'px';
+ 
   }
 
   this.moveBox = function() {
@@ -46,22 +49,69 @@ function Box(x, y, parentEle,xVel, yVel){
     this.y += this.yVel;
   }
 
-  this.update = function(x , y) {
-    this.xVel = x;
-    this.yVel = y;
+  this.update = function(rawTheta) {
+    this.xVel = this.xVel * Math.cos(rawTheta);
+    this.yVel = this.yVel * Math.sin(rawTheta);
+    console.log('update ' + xVel, yVel);
   }
 
-  this.checkCollision = function() {
-    // console.log(this.x);
-    // console.log(this.y);
-
+  this.checkBoundaryCollision = function() {
     if (this.x + BOXWIDTH >= WRAPPERWIDTH || this.x-BOXWIDTH <= 0 ){
       this.xVel = -this.xVel ;
     }
-
     else if( this.y + BOXHEIGHT >= WRAPPERHEIGHT || this.y - BOXHEIGHT <= 0 ) {
       this.yVel = -this.yVel;
     }
+  }
+  this.angle = function() {
+    return (-this.y) / this.x;
+  }
+
+  this.distance = function (v1,v2) {
+    console.log('from distance cacl this' + this);
+    return Math.sqrt(Math.pow((v2[1] - v1[1]),2) + Math.pow((v2[0]-v1[0]),2));
+    
+  }
+
+  this.checkCollision = function(obj) {
+    const VECTEXT1 = [[this.x , this.y] , [this.x +BOXWIDTH , this.y],
+    [this.x , this.y+BOXHEIGHT],
+     [this.x +BOXWIDTH, this.y+BOXHEIGHT]]
+
+   
+    const VECTEXT2 = [[obj.x , obj.y],
+    [obj.x +BOXWIDTH , obj.y],
+    [obj.x , obj.y+BOXHEIGHT],
+     [obj.x +BOXWIDTH, obj.y+BOXHEIGHT]]
+     var mag=[]
+
+     for(var i in [0,1,2,3]){
+mag.push(this.distance(VECTEXT1[i],VECTEXT2[i]));
+    }
+    console.log(mag);
+    console.log(Math.min(...mag))
+    
+    if (Math.min(...mag)<DIAGONAL){
+      console.log('collison mini')
+      this.xVel = -this.xVel;
+      this.yVel = -this.yVel;  
+      obj.xVel = -obj.xVel;
+      obj.yVel = -obj.yVel;
+      return true;
+    }
+return false;
+    // console.log('from check collision')
+    // var dis = this.distance(obj);
+    // var rawTheta = (this.x-obj.x)/dis;
+    // console.log('befoe = ' +this.xVel);
+    // // this.update(rawTheta);
+    // // obj.update(rawTheta);
+    // console.log('after = ' + this.xVel);
+
+    // if (this.distance(obj) <= this.BOXWIDTH + obj.BOXWIDTH ) {
+      
+    // }
+
   }
 }
 
@@ -70,23 +120,40 @@ function Animation(parent) {
   
   this.init = function() {
     for (var i=0; i<TOTALBOX; i++){
-      var box= new Box(getRandomArbitrary(0, WRAPPERWIDTH-BOXWIDTH), getRandomArbitrary(0, WRAPPERHEIGHT-BOXHEIGHT),parent, getRandomArbitrary(1,0),getRandomArbitrary(1,0));
+      var box= new Box(getRandomArbitrary(0, WRAPPERWIDTH-BOXWIDTH), getRandomArbitrary(0, WRAPPERHEIGHT-BOXHEIGHT),parent, getRandomArbitrary(1,3),getRandomArbitrary(1,3));
       boxes.push(box);
       box.init();
       box.draw();
     }
-    setInterval(this.animate.bind(this), 10/60);
+    setInterval(this.animate.bind(this), 30);
+    // requestAnimationFrame(() => this.animate());
   }
   this.animate = function() {
+
     for (var i= 0; i < boxes.length; i++){
-        var box = boxes[i];
-        box.moveBox();
-        box.checkCollision();
+    
+        boxes[i].moveBox();
         boxes[i].draw();
+    }
+    
+    for (var i= 0; i < boxes.length; i++){
+      const box = boxes[i];
+        // const other = boxes.slice(i + 1);
+        // console.log(other);
+        
+      for (var j =0; j < boxes.length; j++){  
+      // for ( var b of other) {
+          if ( i  != j ){
+            box.checkCollision(boxes[j])
+            
+        }
       }
+        box.checkBoundaryCollision();
+     
+   
     }
   }
-
+}
 new Animation(wrapper).init();
 
 
